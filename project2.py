9b927d4fc323e566dc5d1fcb34c6e6c7cae8183b
Hunter Westerlund <hunterw.mail@gmail.com>
2021-11-16 15:08:50 -0600

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,7 +1,11 @@
 # Hunter Westerlund and Charlotte Vance
 # COMP 5350 Project 2
 
-
+#imports
 import sys
 
-print("Hello World")
+
+# this is where the name of the disk image will come in
+filename = sys.argv[1]
+
+print("it worked")
