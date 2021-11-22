090cf33b919a701234afe7eb46d6814a23475ddc
Hunter Westerlund <hunterw.mail@gmail.com>
2021-11-22 01:28:49 -0600

    I'm trying to fix the gifs
    GIFs are really hard to figure out where the end of the file is. If you just look for the EOF marker they can get cut off in the middle so you have to iterate through the file to find the EOF marker where it's actually supposed to be. This is painful.

--- project2.py
+++ project2.py
@@ -6,18 +6,21 @@
 # https://stackoverflow.com/questions/27001419/how-to-append-to-bytes-in-python-3
 # https://stackoverflow.com/questions/19210414/byte-array-to-hex-string
 # https://www.geeksforgeeks.org/file-handling-python/
-# https://id3.org/id3v2.3.0
+# https://www.quickprogrammingtips.com/python/how-to-calculate-sha256-hash-of-a-file-in-python.html
+# https://www.ntfs.com/gif-signature-format.htm
+# http://giflib.sourceforge.net/whatsinagif/bits_and_bytes.html
 
 # Recovery Steps
 # 1. Scan disk image to find index of file header which is the start offset
-# 2. Use the properties of the header to find the file size of that file
-# 3. Use the file size to calculate the end offset
+# 2. Use the properties of the header to find the file size of that file or find the footer
+# 3. Use the file size/footer location to calculate the end offset
 # 4. Collect the specified bytes and write those to a file
+# 5. Find the SHA-256 of the bytes
 
 #imports
 import sys
 import os
-import struct
+import hashlib
 
 # this is where the name of the disk image will come in
 filename = sys.argv[1]
@@ -27,6 +30,7 @@ filename = sys.argv[1]
 image_size = os.path.getsize(filename)
 
 # MPG
+# TODO make this one work
 # I cannot figure this one out
 def MPGRecovery():
     print('\nMPG File locations:')
@@ -91,7 +95,79 @@ def BMPRecovery():
     return 0
 
 #GIF
+# GIFs are weird. These are animated gifs and so we need to find every frame of the animation all the way to the end of the file. We have to do this iteratively
+# Steps:
+# 1. Find the file starting locations like normal
+# 2. If the GCT is present then we have to & it with 
 def GIFRecovery():
+    print('\nGIF Files:\n')
+    with open(filename, 'rb') as f:
+
+        s = f.read()
+        count = 0
+        index = 0
+        try:
+            while True:
+                # Locating the GIF89a header then checking whether it is at the start of a sector
+                index = s.index(b'\x47\x49\x46\x38\x39\x61', index)
+                if(index % 512 != 0):
+                    break
+                print('Start Offset: ' + hex(index))
+
+                # # Finding File Size
+                # offset = 0
+                # end_index = 0
+                # # Getting offset of the first block from the file header information
+                # if(s[index + 0x0A] >= 0b10000000):
+                #     offset = (1 <<((s[index + 0x0A] & 0x09) + 1)) * 3
+                # offset = offset + 13
+                
+                # # This interates through the gif looking for the 0x3B that signifies the end of the file
+                # # The reason it has to be done this way is because a GIF works in blocks of data so the 0x3B has to be at the beginning of a block
+                # # Blocks are calculated based on the locations of other blocks and sub-blocks
+                # while end_index == 0:
+
+                #     # Extension blocks
+                #     if(s[offset:offset + 2] == 0x2101):
+                #         offset = offset + 2
+                #     if(s[offset:offset + 2] == 0x21F9):
+                #         offset = offset + 2
+                #     if(s[offset:offset + 2] == 0x21FF):
+                #         offset = offset + 2
+                #     # Image block - can contain header which is the first if, otherwise it can have a variable number of sub-blocks which the second if calculates
+                #     elif(s[offset] == 0x2C):
+                #         if(s[offset + 0x0B] >= 0b10000000):
+                #             offset = (1 <<((hex(s[offset + 0x0B]) & 0x07) + 1)) * 3
+                #         else:
+                #             offset = offset + 0x0B + 1
+                #             while s[offset] != 0x00:
+                #                 offset = offset + s[offset]
+                #             offset = offset + 1
+                #     # EOF marker - set end_index and break loop
+                #     elif(s[offset] == 0x3B):
+                #         end_index = offset
+                #     else:
+                #         print('Marker Not Found at ' + hex(offset))
+                #         break
+
+                end_index = s.index(b'\x00\x3B', index)
+                print('End Offset: ' + hex(end_index))
+
+                # Writing the file
+                written_file = open("gif-" + str(count) + ".gif", "wb")
+                written_file.write(s[index:end_index + 1])
+                written_file.close()
+                print('File Written')
+
+                # Hashing the bytes which make up the file
+                hash = hashlib.sha256(s[index:end_index + 1]).hexdigest()
+                print('SHA-256: ' + hash)
+                index += 4
+                count += 1
+                print()
+        except ValueError:
+            print("EOF")
+        print('Found ' + str(count) + ' files')
     return 0
 
 #JPG
@@ -104,39 +180,42 @@ def DOCXRecovery():
 
 #AVI
 def AVIRecovery():
-    print('\nAVI File locations:')
+    print('\nAVI Files:\n')
     with open(filename, 'rb') as f:
-        total = 0
 
         s = f.read()
-
-        print('\nAVI:')
         try:
             index = 0
             count = 0
             while True:
+                # Locating the RIFF header then checking whether it is an AVI and at the start of a sector
                 index = s.index(b'\x52\x49\x46\x46', index)
                 if(index + 8 != s.index(b'\x41\x56\x49\x20\x4C\x49\x53\x54', index)):
                     break
                 if(index % 512 != 0):
                     break
                 print('Start Offset: ' + hex(index))
+
                 # Finding File Size
                 # File size in AVI is bytes 4-7
                 avi_size = int.from_bytes(s[index + 4:index + 8], 'little')
-                print(hex(avi_size))
+                print('End Offset: ' + hex(index + avi_size))
+
+                # Writing the file
                 written_file = open("avi-" + str(count) + ".avi", "wb")
                 written_file.write(s[index:index + avi_size + 1])
                 written_file.close()
                 print('File Written')
+
+                # Hashing the bytes which make up the file
+                hash = hashlib.sha256(s[index:index + avi_size + 1]).hexdigest()
+                print('SHA-256: ' + hash)
                 index += 4
                 count += 1
+                print()
         except ValueError:
             print("EOF")
         print('Found ' + str(count) + ' files')
-        total += count
-        print('\nTotal AVI types found: ' + str(count))
-
     return 0
 
 #PNG
@@ -146,7 +225,7 @@ def PNGRecovery():
 #Runner code
 
 print('File size is: ' + str(hex(image_size)) + ' bytes')
-MPGRecovery()
+# MPGRecovery()
 PDFRecovery()
 BMPRecovery()
 GIFRecovery()
